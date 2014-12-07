8c881e48bbbacb336f442de7f3d6cfa4c9ebe8c1
meta-system <meta_system@online.de>
2014-12-07 19:21:15 +0100

    added P11

---
+++ pages/src/main/P11.java
@@ -0,0 +1,21 @@
+package main;
+
+public class P11 {
+	
+	public void main(){
+		
+		System.out.println("========== Anfang Seite 11. ==========");
+		
+		int x = 4;
+		while (x > 3){
+			x = x - 1;		
+		}
+		int z = 27;
+		while (z == 17){
+			/// wird nicht ausgeführt
+		}
+		
+		System.out.println("=========== Ende Seite 11. ===========");
+		System.out.println();
+	}
+}
