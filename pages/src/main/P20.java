23f8a297ae1bd32d08d6561a3bdd0fdd77f8638f
meta-system <meta_system@online.de>
2014-12-07 20:19:55 +0100

    Added P20

---
+++ pages/src/main/P20.java
@@ -0,0 +1,31 @@
+package main;
+
+public class P20 {
+	
+	public void main(){
+		System.out.println("========== Anfang Seite 20. ==========");
+		
+		int x = 3;
+		while (x > 0){
+			if (x > 2){
+				System.out.print("a");
+			}
+			x = x - 1;
+				System.out.print("-");
+			if (x == 2){
+				System.out.print("b c");
+			}
+			x = x - 1;
+			System.out.print("-");
+			if (x == 1){
+				System.out.print("d");
+				x = x - 1;
+			}
+		}
+		
+		System.out.println();
+		System.out.println("=========== Ende Seite 20. ===========");
+		System.out.println();
+	}
+
+}
