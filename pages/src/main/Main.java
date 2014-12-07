4dbecc2c4956beda5c3a2bbd78ef29b2a5d8424b
meta-system <meta_system@online.de>
2014-12-07 19:07:35 +0100

    added pages

--- pages/src/main/Main.java
+++ pages/src/main/Main.java
@@ -2,4 +2,12 @@ package main;
 
 public class Main {
 
+	public static void main(String[] args) {
+		P10 output10 = new P10();
+		output10.main();
+		
+		
+
+	}
+
 }
--- pages/src/main/P10.java
+++ pages/src/main/P10.java
@@ -1,5 +1,40 @@
 package main;
 
 public class P10 {
+	
+	public void main(){
+		System.out.println("========== Anfang Seite 10. ==========");
+		
+		int x = 3;
+		String name = "Dirk";
+		x = x * 17;
+		System.out.println("x ist  " + x);
+		double d = Math.random();
+		
+		/// das ist ein Kommentar
+		System.out.println("// Eigentlich wird d nicht genutzt, aber so: " + d);
+		
+		while (x > 12){
+			x = x - 1;
+		}
+		for (x = 0; x < 10; x++){
+			System.out.println("x ist jetzt " + x);
+		}
+		
+		
+		if (x == 10) {
+			System.out.println("x ist 10");
+		} else {
+			System.out.println("ist nicht 10");
+		}
+		if ((x<3) & (name.equals("Dirk"))) {
+			System.out.println("Vorsicht!");
+		}
+		System.out.println("Diese Zeile wird immer ausgeführt.");
+		
+		
+		System.out.println("=========== Ende Seite 10. ===========");
+		System.out.println();
+	}
 
 }
