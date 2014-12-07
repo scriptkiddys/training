17fbe5876dc9480d29bcd57310cd5c5c3ab4afff
meta-system <meta_system@online.de>
2014-12-07 19:22:16 +0100

    added P12

--- pages/src/main/P12.java
+++ pages/src/main/P12.java
@@ -1,5 +1,22 @@
 package main;
 
 public class P12 {
+	
+	public void main(){
+		System.out.println("========== Anfang Seite 12. ==========");
+		
+		int x = 1;
+		System.out.println("Vor der Schleife");
+		while (x < 4) {
+			System.out.println("In der Schleife");
+			System.out.println("Der Wert von x ist " + x);
+			x = x + 1;
+		}
+		
+		System.out.println("Nach der Schleife");
+		
+		System.out.println("=========== Ende Seite 12. ===========");
+		System.out.println();
+	}
 
 }
